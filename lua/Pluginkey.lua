76470d0f7f90c26aa1e8d63959a70377ff5d142a
figman12 <167013417+figman12@users.noreply.github.com>
2025-02-10 20:10:32 +0100

    Update Pluginkey.lua

--- lua/Pluginkey.lua
+++ lua/Pluginkey.lua
@@ -1,8 +1,14 @@
-local authkey = "Ispebdev"
+if button then
+    local authkey = "Ispebdev"
 
-local function auth()
-	plugin:SetSetting("PEBDEV_vC1TFjvr5mtVuIsw7gzJsvkPAjnhg9Oj", authkey)	
-	print("Authorization Done!")
+    local function auth()
+        -- Set the plugin setting when authorized
+        plugin:SetSetting("PEBDEV_vC1TFjvr5mtVuIsw7gzJsvkPAjnhg9Oj", authkey)
+        print("Authorization Done!")
+    end
+
+    -- Connect the click event to the auth function
+    button.Click:Connect(auth)
+else
+    warn("Button creation failed.")
 end
-
-button.Click:Connect(auth)
